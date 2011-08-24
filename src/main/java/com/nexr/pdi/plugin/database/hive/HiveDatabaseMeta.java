431d4ef02d1ba2baeb943f48a54fba5de720f22a
Brandon, Youngwoo Kim <brandon.kim@nexr.com>
2011-08-24 10:48:49 +0900

    Reformatting source code

--- src/main/java/com/nexr/pdi/plugin/database/hive/HiveDatabaseMeta.java
+++ src/main/java/com/nexr/pdi/plugin/database/hive/HiveDatabaseMeta.java
@@ -8,228 +8,231 @@ import org.pentaho.di.core.plugins.DatabaseMetaPlugin;
 import org.pentaho.di.core.row.ValueMetaInterface;
 
 @DatabaseMetaPlugin(type = "HADOOP HIVE", typeDescription = "Hadoop Hive")
-public class HiveDatabaseMeta extends BaseDatabaseMeta implements
-		DatabaseInterface {
+public class HiveDatabaseMeta extends BaseDatabaseMeta implements DatabaseInterface {
 
-	public String getDriverClass() {
+    public String getDriverClass() {
 
-		return "org.apache.hadoop.hive.jdbc.HiveDriver";
-	}
+        return "org.apache.hadoop.hive.jdbc.HiveDriver";
+    }
 
-	public String getURL(String hostname, String port, String databaseName)
-			throws KettleDatabaseException {
+    public String getURL(String hostname, String port, String databaseName)
+            throws KettleDatabaseException {
 
-		return "jdbc:hive://" + hostname + ":" + port + "/" + databaseName;
-	}
+        return "jdbc:hive://" + hostname + ":" + port + "/" + databaseName;
+    }
 
-	public String getAddColumnStatement(String tablename, ValueMetaInterface v,
-			String tk, boolean use_autoinc, String pk, boolean semicolon) {
-		return "ALTER TABLE " + tablename + " ADD "
-				+ getFieldDefinition(v, tk, pk, use_autoinc, true, false);
-	}
+    public String getAddColumnStatement(String tablename, ValueMetaInterface v, String tk,
+            boolean use_autoinc, String pk, boolean semicolon) {
+        return "ALTER TABLE " + tablename + " ADD "
+                + getFieldDefinition(v, tk, pk, use_autoinc, true, false);
+    }
 
-	public String getModifyColumnStatement(String tablename,
-			ValueMetaInterface v, String tk, boolean use_autoinc, String pk,
-			boolean semicolon) {
-		return "ALTER TABLE " + tablename + " MODIFY "
-				+ getFieldDefinition(v, tk, pk, use_autoinc, true, false);
-	}
+    public String getModifyColumnStatement(String tablename, ValueMetaInterface v, String tk,
+            boolean use_autoinc, String pk, boolean semicolon) {
+        return "ALTER TABLE " + tablename + " MODIFY "
+                + getFieldDefinition(v, tk, pk, use_autoinc, true, false);
+    }
 
-	public String[] getUsedLibraries() {
-		return new String[] { "" };
-	}
+    public String[] getUsedLibraries() {
+        return new String[] { "" };
+    }
 
-	@Override
-	public int[] getAccessTypeList() {
+    @Override
+    public int[] getAccessTypeList() {
 
-		return new int[] { DatabaseMeta.TYPE_ACCESS_NATIVE };
-	}
+        return new int[] { DatabaseMeta.TYPE_ACCESS_NATIVE };
+    }
 
-	@Override
-	public int getDefaultDatabasePort() {
-		if (getAccessType() == DatabaseMeta.TYPE_ACCESS_NATIVE) {
-			return 10000;
-		} else {
-			return -1;
-		}
-	}
-	
-	public String getFieldDefinition(ValueMetaInterface v, String tk,
-			String pk, boolean use_autoinc, boolean add_fieldname,
-			boolean add_cr) {
-		String retval = "";
+    @Override
+    public int getDefaultDatabasePort() {
+        if (getAccessType() == DatabaseMeta.TYPE_ACCESS_NATIVE) {
+            return 10000;
+        } else {
+            return -1;
+        }
+    }
 
-		String fieldname = v.getName();
-		int length = v.getLength();
-		int precision = v.getPrecision();
+    public String getFieldDefinition(ValueMetaInterface v, String tk, String pk,
+            boolean use_autoinc, boolean add_fieldname, boolean add_cr) {
+        String retval = "";
 
-		if (add_fieldname) {
-			retval += fieldname + " ";
-		}
+        String fieldname = v.getName();
+        int length = v.getLength();
+        int precision = v.getPrecision();
 
-		int type = v.getType();
-		switch (type) {
+        if (add_fieldname) {
+            retval += fieldname + " ";
+        }
 
-		case ValueMetaInterface.TYPE_BOOLEAN:
-			retval += "BOOLEAN";
-			break;
+        int type = v.getType();
+        switch (type) {
 
-		// Hive does not support DATE
-		case ValueMetaInterface.TYPE_DATE:
-			retval += "STRING";
-			break;
+        case ValueMetaInterface.TYPE_BOOLEAN:
+            retval += "BOOLEAN";
+            break;
 
-		case ValueMetaInterface.TYPE_STRING:
-			retval += "STRING";
-			break;
+        // Hive does not support DATE
+        case ValueMetaInterface.TYPE_DATE:
+            retval += "STRING";
+            break;
 
-		case ValueMetaInterface.TYPE_NUMBER:
-		case ValueMetaInterface.TYPE_INTEGER:
-		case ValueMetaInterface.TYPE_BIGNUMBER:
-			// Integer values...
-			if (precision == 0) {
-				if (length > 9) {
-					if (length < 19) {
-						// can hold signed values between -9223372036854775808
-						// and 9223372036854775807
-						// 18 significant digits
-						retval += "BIGINT";
-					} else {
-						retval += "FLOAT";
-					}
-				} else {
-					retval += "INT";
-				}
-			}
-			// Floating point values...
-			else {
-				if (length > 15) {
-					retval += "FLOAT";
-				} else {
-					// A double-precision floating-point number is accurate to
-					// approximately 15 decimal places.
-					// http://mysql.mirrors-r-us.net/doc/refman/5.1/en/numeric-type-overview.html
-					retval += "DOUBLE";
-				}
-			}
+        case ValueMetaInterface.TYPE_STRING:
+            retval += "STRING";
+            break;
 
-			break;
-		}
+        case ValueMetaInterface.TYPE_NUMBER:
+        case ValueMetaInterface.TYPE_INTEGER:
+        case ValueMetaInterface.TYPE_BIGNUMBER:
+            // Integer values...
+            if (precision == 0) {
+                if (length > 9) {
+                    if (length < 19) {
+                        // can hold signed values between -9223372036854775808
+                        // and 9223372036854775807
+                        // 18 significant digits
+                        retval += "BIGINT";
+                    } else {
+                        retval += "FLOAT";
+                    }
+                } else {
+                    retval += "INT";
+                }
+            }
+            // Floating point values...
+            else {
+                if (length > 15) {
+                    retval += "FLOAT";
+                } else {
+                    // A double-precision floating-point number is accurate to
+                    // approximately 15 decimal places.
+                    // http://mysql.mirrors-r-us.net/doc/refman/5.1/en/numeric-type-overview.html
+                    retval += "DOUBLE";
+                }
+            }
 
-		return retval;
-	}
+            break;
+        }
 
-	@Override
-	public boolean supportsAutoInc() {
-		return false;
-	}
+        return retval;
+    }
 
-	@Override
-	public String getLimitClause(int nrRows) {
+    @Override
+    public boolean supportsAutoInc() {
+        return false;
+    }
 
-		return " LIMIT " + nrRows;
-	}
+    @Override
+    public String getLimitClause(int nrRows) {
 
-	@Override
-	public boolean isFetchSizeSupported() {
+        return " LIMIT " + nrRows;
+    }
 
-		return true;
-	}
+    @Override
+    public boolean isFetchSizeSupported() {
 
-	@Override
-	public boolean supportsTransactions() {
+        return true;
+    }
 
-		return false;
-	}
+    @Override
+    public boolean supportsTransactions() {
 
-	@Override
-	public boolean supportsSequences() {
+        return false;
+    }
 
-		return false;
-	}
+    @Override
+    public boolean supportsSequences() {
 
-	@Override
-	public String[] getReservedWords() {
+        return false;
+    }
 
-		return new String[] { "ADD", "ALL", "ALTER", "AND", "AS", "ASC", "ARRAY",
-				"BEFORE", "BETWEEN", "BIGINT", "BY", "CASE", "COLUMN", "COLUMNS", "COMMENT", "COLLECTION",
-				"CREATE", "CROSS", "CLUSTER", "CLUSTERED", "DATABASE", "DATABASES", "DESC", "DEFAULT",
-				"DESCRIBE", "DISTRIBUTE", "DELIMITED", "DIRECTORY", "DISTINCT",
-				"DOUBLE", "DROP", "EXISTS", "EXPLAIN", "EXTENDED", "EXTERNAL", "FLOAT",
-				"FORMAT", "FROM", "FIELDS", "GRANT", "GROUP", "HAVING", "IF",
-				"IN", "INDEX", "INNER", "INOUT", "INSENSITIVE", "INSERT", "ITEMS",
-				"INT", "INTO", "IS", "ITERATE", "JOIN",
-				"KEYS", "LEFT", "LIKE", "LIMIT", "LOAD", "LOCATION", "LOCK", "LINES",
-				"MAP", "NOT", "NULL", "ON", "OR", "ORDER", "OUTER",
-				"OVERWRITE", "PARTITION", "PARTITIONED", "REGEXP", "ROW", "REDUCE", "RENAME", "REPLACE",
-				"REVOKE", "RIGHT", "RLIKE", "SELECT", "SET", "SHOW", "SORT",
-				"SMALLINT", "STRUCT", "STORED", "STRING", "TABLE",
-				"TERMINATED", "TINYINT", "TRANSFORM", "THEN", "UNION", "USE",
-				"USING", "WHEN", "WHERE" };
-	}
+    @Override
+    public String[] getReservedWords() {
 
-	@Override
-	public boolean supportsRepository() {
+        return new String[] { "ADD", "ALL", "ALTER", "AND", "AS", "ASC", "ARRAY", "BEFORE",
+                "BETWEEN", "BIGINT", "BY", "CASE", "COLUMN", "COLUMNS", "COMMENT", "COLLECTION",
+                "CREATE", "CROSS", "CLUSTER", "CLUSTERED", "DATABASE", "DATABASES", "DESC",
+                "DEFAULT", "DESCRIBE", "DISTRIBUTE", "DELIMITED", "DIRECTORY", "DISTINCT",
+                "DOUBLE", "DROP", "EXISTS", "EXPLAIN", "EXTENDED", "EXTERNAL", "FLOAT", "FORMAT",
+                "FROM", "FIELDS", "GRANT", "GROUP", "HAVING", "IF", "IN", "INDEX", "INNER",
+                "INOUT", "INSENSITIVE", "INSERT", "ITEMS", "INT", "INTO", "IS", "ITERATE", "JOIN",
+                "KEYS", "LEFT", "LIKE", "LIMIT", "LOAD", "LOCATION", "LOCK", "LINES", "MAP", "NOT",
+                "NULL", "ON", "OR", "ORDER", "OUTER", "OVERWRITE", "PARTITION", "PARTITIONED",
+                "REGEXP", "ROW", "REDUCE", "RENAME", "REPLACE", "REVOKE", "RIGHT", "RLIKE",
+                "SELECT", "SET", "SHOW", "SORT", "SMALLINT", "STRUCT", "STORED", "STRING", "TABLE",
+                "TERMINATED", "TINYINT", "TRANSFORM", "THEN", "UNION", "USE", "USING", "WHEN",
+                "WHERE" };
+    }
 
-		return false;
-	}
+    @Override
+    public boolean supportsRepository() {
 
-	@Override
-	public boolean supportsViews() {
+        return false;
+    }
 
-		return true;
-	}
+    @Override
+    public boolean supportsViews() {
 
-	@Override
-	public boolean supportsSynonyms() {
+        return true;
+    }
 
-		return false;
-	}
+    @Override
+    public boolean supportsSynonyms() {
 
-	@Override
-	public String getSQLQueryFields(String tableName) {
+        return false;
+    }
 
-		return "SELECT * FROM " + tableName + " LIMIT 1";
-	}
+    @Override
+    public String getSQLQueryFields(String tableName) {
 
-	@Override
-	public boolean supportsBatchUpdates() {
+        return "SELECT * FROM " + tableName + " LIMIT 1";
+    }
 
-		return false;
-	}
+    @Override
+    public boolean supportsBatchUpdates() {
 
-	@Override
-	public boolean supportsBooleanDataType() {
+        return false;
+    }
 
-		return false;
-	}
+    @Override
+    public boolean supportsBooleanDataType() {
 
-	@Override
-	public String getSQLTableExists(String tablename) {
+        return false;
+    }
 
-		return getSQLQueryFields(tablename);
-	}
+    @Override
+    public String getSQLTableExists(String tablename) {
 
-	@Override
-	public boolean isExplorable() {
+        return getSQLQueryFields(tablename);
+    }
 
-		return true;
-	}
+    public String getSQLColumnExists(String columnname, String tablename) {
+        return getSQLQueryColumnFields(columnname, tablename);
+    }
 
-	@Override
-	public String getXulOverlayFile() {
+    public String getSQLQueryColumnFields(String columnname, String tableName) {
+        return "SELECT " + columnname + " FROM " + tableName + getLimitClause(1);
+    }
 
-		return "comon";
-	}
+    @Override
+    public boolean isExplorable() {
 
-	@Override
-	public String getSelectCountStatement(String tableName) {
+        return true;
+    }
 
-		return "SELECT count(*) FROM " + tableName;
-	}
+    @Override
+    public String getXulOverlayFile() {
 
-	public String getExtraOptionsHelpText() {
-		return "http://hive.apache.org/";
-	}
+        return "comon";
+    }
+
+    @Override
+    public String getSelectCountStatement(String tableName) {
+
+        return "SELECT count(*) FROM " + tableName;
+    }
+
+    public String getExtraOptionsHelpText() {
+        return "http://hive.apache.org/";
+    }
 
 }
